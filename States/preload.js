b18fc21b89a9564239170838e0cbd019c3e4413f
RBrNx277 <b00231929@studentmail.uws.ac.uk>
2015-03-24 13:45:47 +0000

    Added backgrounds

--- States/preload.js
+++ States/preload.js
@@ -8,7 +8,9 @@ preload.prototype = {
             var loadingBar = this.add.sprite(this.game.world.width/2, this.game.world.height/2, "Loading");
             loadingBar.anchor.setTo(0.5,0.5);
             this.load.setPreloadSprite(loadingBar);
-        this.game.load.image("Logo", "Graphics/Logo.png")
+        this.game.load.image("Logo", "Graphics/Logo.png");
+        this.game.load.image("Clouds", "Graphics/Background/Background-Clouds.png");
+        this.game.load.image("Hills", "Graphics/Background/Background-Hills.png");
     },
     create: function(){
         this.game.state.start("MainMenu");
